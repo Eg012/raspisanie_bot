6d3fcad8ebdd60d853e4263109c013413b6b6f69
Egor <savinovego05@gmail.com>
2022-03-09 19:45:44 +0300

    пофиксил название файла

--- bot.py
+++ bot.py
@@ -58,12 +58,7 @@ def main():
 
     # log all errors
     dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=8443,
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://hidden-coast-63167.herokuapp.com' + TOKEN)
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
